906e8648d7f8479e480af818ee7db117226027b4
Hyoeun Lee <scarlettcello@gmail.com>
2018-09-12 21:15:07 -0400

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect MongoDB server');
+    }
+    console.log('Connected to MongoDB');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5b98956ea8c134869f11ed60')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5b974e65687928593817b311')
+    }, {
+        $set: {
+            name: 'Carol',
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+    //db.close();
+});
