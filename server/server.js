53ffa321d91974eb39210ceddc94a897ed5acd19
Hyoeun Lee <scarlettcello@gmail.com>
2018-09-16 20:51:30 -0400

    Add tests for DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -127,7 +127,7 @@ app.delete('/users/me/token', authenticate, (req, res) => {
     req.user.removeToken(req.token).then(() => {
         res.status(200).send();
     }, () => {
-        res.statis(400).send();
+        res.status(400).send();
     });
 });
 
